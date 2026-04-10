33966f7ad2d647fd379e4742f8679ba656e40046
David Silva <daavvid157@gmail.com>
2026-04-10 14:45:53 -0400

    primeiro commit bot doceria

---
+++ bot.js
@@ -0,0 +1,95 @@
+const wppconnect = require('@wppconnect-team/wppconnect');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req,res)=>{
+    res.send("Bot da doceria rodando 🍰");
+});
+
+app.listen(PORT, ()=>{
+    console.log("Servidor rodando na porta " + PORT);
+});
+
+wppconnect.create({
+    puppeteerOptions: {
+  args: ['--no-sandbox', '--disable-setuid-sandbox']
+},
+    session: 'doceria',
+    catchQR: (base64Qr, asciiQR) => {
+        console.log("ESCANEIE O QR CODE");
+        console.log(asciiQR);
+    }
+})
+.then(client => start(client))
+.catch(error => console.log(error));
+
+function start(client){
+
+client.onMessage(async (message)=>{
+
+if(message.fromMe) return;
+
+const msg = message.body.toLowerCase();
+
+if(msg.length > 0){
+
+await client.sendText(message.from,
+
+`🍰 *Melu Doceria*
+
+Escolha uma opção:
+
+1️⃣ Cardápio
+2️⃣ encomendas personalizadas 
+3️⃣ Horário de atendimento
+4️⃣ Atendimento
+5 instagram`);
+
+ 
+}
+
+if(msg === "1"){
+
+await client.sendText(message.from,
+
+`🍩 Cardápio
+
+https://wa.me/c/556793464156`);
+
+}
+
+if(msg === "2"){
+
+await client.sendText(message.from,
+
+`https://wa.me/c/556793464156`);
+
+}
+
+if(msg === "3"){
+
+await client.sendText(message.from,
+
+`Horário:
+
+Segunda a sábado
+11h às 00h
+Domingo 
+13h às 22h`);
+
+}
+if(msg === "5"){
+
+await client.sendText(message.from,
+
+`instagram
+
+https://www.instagram.com/meludoceriaa/`);
+
+}
+
+});
+
+}
