cad954c4011efe72f667733b55a285fd56afbf93
David-dev0 <daavvid157@gmail.com>
2026-04-13 19:29:48 -0400

    "else e if"

--- bot.js
+++ bot.js
@@ -65,7 +65,7 @@ Digite um número para escolher a opção desejada ou digite *0* para voltar ao 
 }
 
 //se for texto
-if (["0"].includes(msg)) {
+if (msg === "0") {
     clientes[numero] = { etapa: "menu" };
  return client.sendText(numero,
 `🍰 *Melu Doceria*
